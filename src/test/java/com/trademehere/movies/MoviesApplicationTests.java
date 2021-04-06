bbdde6861346fdcd047e7773ecac439a89929462
Marcial Rivas <marcial.rivas@gmail.com>
2021-04-06 14:53:14 -0500

    # feat: Create HelloWorldControllerIntegrationTest and basic MoviesApplicationTest

---
+++ src/test/java/com/trademehere/movies/MoviesApplicationTests.java
@@ -0,0 +1,22 @@
+package com.trademehere.movies;
+
+import com.trademehere.movies.controller.api.HelloWorldController;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MoviesApplicationTests {
+
+    @Autowired
+	private HelloWorldController helloWorldController;
+
+	@Test
+	public void contextLoads() {
+        assertThat(helloWorldController).isNotNull();
+	}
+
+}
