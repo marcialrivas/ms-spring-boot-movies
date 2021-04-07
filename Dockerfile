3c26dd367b02046508c431a00d2eb7773407ebff
mpedroza <mpedroza@COR1ECOQSBE.pricecorp.priceshoes.com.mx>
2021-04-07 14:49:59 -0500

    Using env variables.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,19 @@
 FROM openjdk:11-buster
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]
+COPY filebeat.yml /filebeat.yml
+RUN set -x && \
+      apt-get update && \
+      apt-get install -y wget && \
+      wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.12.0-linux-x86_64.tar.gz -O /opt/filebeat.tar.gz && \
+      cd /opt && \
+      tar xzvf filebeat.tar.gz && \
+      cd filebeat-* && \
+      cp filebeat /bin && \
+      cd /opt && \
+      rm -rf filebeat* && \
+      apt-get purge -y wget && \
+      apt-get autoremove -y && \
+      apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+ENTRYPOINT ["/bin/sh", "-c" , "nohup filebeat -e & java -jar /app.jar"]
+EXPOSE 8080
--- src/main/java/com/trademehere/movies/MoviesApplication.java
+++ src/main/java/com/trademehere/movies/MoviesApplication.java
@@ -1,12 +1,27 @@
 package com.trademehere.movies;
 
+import java.util.HashMap;
+
 import org.springframework.boot.SpringApplication;
+import co.elastic.apm.attach.ElasticApmAttacher;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MoviesApplication {
 
 	public static void main(String[] args) {
+		
+		HashMap<String, String> configuration = new HashMap<String, String>();
+		configuration.put("service_name", System.getenv("ELASTIC_APM_SERVICE_NAME"));
+		configuration.put("server_urls", System.getenv("ELASTIC_APM_SERVERS_URL"));
+		configuration.put("secret_token", System.getenv("ELASTIC_APM_SECRET_TOKEN"));
+		configuration.put("application_packages", "com.trademehere.movies");
+		configuration.put("environment", System.getenv("ELASTIC_APM_ENVIRONMENT"));
+		configuration.put("enable_log_correlation","true");
+		
+		
+		ElasticApmAttacher.attach(configuration);
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
