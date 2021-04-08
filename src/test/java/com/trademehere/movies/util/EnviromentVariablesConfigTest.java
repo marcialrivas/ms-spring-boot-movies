786ca0fed33bc00da8a456deb338c519682ca978
mpedroza <mpedroza@COR1ECOQSBE.pricecorp.priceshoes.com.mx>
2021-04-07 23:49:13 -0500

    Fix test case for EnvironmentVariables class

--- src/test/java/com/trademehere/movies/util/EnviromentVariablesConfigTest.java
+++ src/test/java/com/trademehere/movies/util/EnviromentVariablesConfigTest.java
@@ -1,34 +1,23 @@
 package com.trademehere.movies.util;
 
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
-import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
-import uk.org.webcompere.systemstubs.jupiter.SystemStub;
-import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 
-@ExtendWith(SystemStubsExtension.class)
+
 class EnviromentVariablesConfigTest {
 
-	@SystemStub
-	public final EnvironmentVariables environmentVariables = new EnvironmentVariables();
-
-	@BeforeEach
-	void setUp() throws Exception {
-		environmentVariables.set("ELASTIC_APM_SERVICE_NAME", "SERVICE NAME");
-		environmentVariables.set("ELASTIC_APM_SERVERS_URL", "http://localhost");
-		environmentVariables.set("ELASTIC_APM_SECRET_TOKEN", "TOKEN");
-		environmentVariables.set("ELASTIC_APM_ENVIRONMENT", "ENV");
-	}
 
 	@Test
 	void testGetElasctiAMPConfing() {
-		System.out.println(EnviromentVariablesConfig.getElasctiAMPConfing());
-		assertTrue(EnviromentVariablesConfig.getElasctiAMPConfing().containsValue("TOKEN"));
+		assertEquals(EnviromentVariablesConfig.getElasctiAMPConfing().get("service_name"),"");
+		assertEquals(EnviromentVariablesConfig.getElasctiAMPConfing().get("server_urls"),"");
+		assertEquals(EnviromentVariablesConfig.getElasctiAMPConfing().get("secret_token"),"");
+		assertEquals(EnviromentVariablesConfig.getElasctiAMPConfing().get("environment"),"");
+		assertEquals(EnviromentVariablesConfig.getElasctiAMPConfing().get("enable_log_correlation"),"true");
+		assertEquals(EnviromentVariablesConfig.getElasctiAMPConfing().get("application_packages"),"com.trademehere.movies");
 	}
 
 }
