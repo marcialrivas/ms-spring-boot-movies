cee9a48531c88d85729d52190add004bebb60854
mpedroza <mpedroza@COR1ECOQSBE.pricecorp.priceshoes.com.mx>
2021-04-07 23:55:37 -0500

    # Bug: EnvironmentVariablesConfig
    * Fix env variables per item in map
    * Add test cases to cover all conditions

--- src/test/java/com/trademehere/movies/util/EnviromentVariablesConfigTest.java
+++ src/test/java/com/trademehere/movies/util/EnviromentVariablesConfigTest.java
@@ -4,11 +4,17 @@ package com.trademehere.movies.util;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
+import uk.org.webcompere.systemstubs.jupiter.SystemStub;
+import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 
-
+@ExtendWith(SystemStubsExtension.class)
 class EnviromentVariablesConfigTest {
 
+	@SystemStub
+	public final EnvironmentVariables environmentVariables = new EnvironmentVariables();
 
 	@Test
 	void testGetElasctiAMPConfing() {
@@ -20,4 +26,16 @@ class EnviromentVariablesConfigTest {
 		assertEquals(EnviromentVariablesConfig.getElasctiAMPConfing().get("application_packages"),"com.trademehere.movies");
 	}
 
+	@Test
+	void testGetElasctiAMPConfingWithStub() {
+		environmentVariables.set("ELASTIC_APM_SERVICE_NAME", "SERVICE NAME");
+		environmentVariables.set("ELASTIC_APM_SERVERS_URL", "http://localhost");
+		environmentVariables.set("ELASTIC_APM_SECRET_TOKEN", "TOKEN");
+		environmentVariables.set("ELASTIC_APM_ENVIRONMENT", "ENV");
+		
+		assertEquals(EnviromentVariablesConfig.getElasctiAMPConfing().get("service_name"),"SERVICE NAME");
+		assertEquals(EnviromentVariablesConfig.getElasctiAMPConfing().get("server_urls"),"http://localhost");
+		assertEquals(EnviromentVariablesConfig.getElasctiAMPConfing().get("secret_token"),"TOKEN");
+		assertEquals(EnviromentVariablesConfig.getElasctiAMPConfing().get("environment"),"ENV");
+	}
 }
