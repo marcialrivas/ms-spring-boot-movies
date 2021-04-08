87b1847cad0fe9c4910178c1139cacebf0397477
mpedroza <mpedroza@COR1ECOQSBE.pricecorp.priceshoes.com.mx>
2021-04-07 23:42:26 -0500

    Add actuator

--- src/main/java/com/trademehere/movies/util/EnviromentVariablesConfig.java
+++ src/main/java/com/trademehere/movies/util/EnviromentVariablesConfig.java
@@ -13,13 +13,12 @@ public class EnviromentVariablesConfig {
 
 	public static Map<String, String> getElasctiAMPConfing(){
 		HashMap<String, String> configuration = new HashMap<>();
-		configuration.put("service_name", System.getenv("ELASTIC_APM_SERVICE_NAME"));
-		configuration.put("server_urls", System.getenv("ELASTIC_APM_SERVERS_URL"));
-		configuration.put("secret_token", System.getenv("ELASTIC_APM_SECRET_TOKEN"));
-		configuration.put("application_packages", "com.trademehere.movies");
-		configuration.put("environment", System.getenv("ELASTIC_APM_ENVIRONMENT"));
-		configuration.put("enable_log_correlation","true");
-		
+			configuration.put("service_name", System.getenv("ELASTIC_APM_SERVICE_NAME") != null ? System.getenv("ELASTIC_APM_SERVICE_NAME") : "");
+			configuration.put("server_urls", System.getenv("ELASTIC_APM_SERVERS_URL") != null ? System.getenv("ELASTIC_APM_SERVICE_NAME") : "");
+			configuration.put("secret_token", System.getenv("ELASTIC_APM_SECRET_TOKEN") != null ? System.getenv("ELASTIC_APM_SERVICE_NAME") : "");
+			configuration.put("application_packages", "com.trademehere.movies");
+			configuration.put("environment", System.getenv("ELASTIC_APM_ENVIRONMENT") != null ? System.getenv("ELASTIC_APM_SERVICE_NAME") : "");
+			configuration.put("enable_log_correlation","true");
 		return configuration;
 	}
 }
